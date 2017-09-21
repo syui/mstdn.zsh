703a2b071f43e176fbcb6f54a6e3b57d7fdc6779
syui <syui@syui.cf>
2017-09-21 16:38:30 +0900

    fix com

--- scpt/delete.zsh
+++ scpt/delete.zsh
@@ -3,7 +3,7 @@ api_option=`cat $json_docs|jq -r '.[]|select(.DELETE)|.[]'|cut -d / -f 4`
 if [ `echo "$api_option"|wc -l` -ne 1 ];then
 	api_option=`echo "$api_option" | $select_command --query statuses`
 fi
-tmp_id=`cat $json_account_status | jq '.[]|{id,content}'|tr -d '{' | tr -d '}' | sed -e '/^$/d' -e 's/"id": //g' -e 's/"content": //g'| perl -pe 's/,\n/,/'|$select_command|cut -d , -f 1|tr -d ' '`
+tmp_id=`cat $json_account_status | jq '.[]|{id,content}'|tr -d '{' | tr -d '}' | sed -e '/^$/d' -e 's/"id": //g' -e 's/"content": //g'| perl -pe 's/,\n/,/'|$select_command|cut -d , -f 1|tr -d ' '|tr -d '"'`
 
 url=$protocol://$host/$api_url/$api_option/$tmp_id
 uri=`cat $json_account_status| jq -r ".[]|{id,content,uri}|select(.id == $tmp_id)"`
