5bf7507f5b200b1c2b6a196d78c19b30387ab381
syui <syui@syui.cf>
2017-10-17 20:20:42 +0900

    fix timeline

--- scpt/timeline_public.zsh
+++ scpt/timeline_public.zsh
@@ -3,7 +3,7 @@ url=$protocol://$host/$api_url/timelines/public
 curl -sSL $url -H "Authorization: Bearer $access_token" >! $j/timelines_public.json
 
 if [ -n "$2" ];then
-	cat $j/timelines_public.json | jq ".[$2].content"
+	cat $j/timelines_public.json | jq -r ".[$2]|{account,content}|{(.account.acct | tostring): .content}|@text"
 else
-	cat $j/timelines_public.json | jq ".[].content"
+	cat $j/timelines_public.json | jq -r ".[]|{account,content}|{(.account.acct | tostring): .content}|@text"
 fi
