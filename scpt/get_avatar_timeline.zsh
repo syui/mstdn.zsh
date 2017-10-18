840f7ab4dc842c9672a2feaa7a74bf8d83d351f9
syui <syui@syui.cf>
2017-10-19 08:45:26 +0900

    fix emoji

--- scpt/get_avatar_timeline.zsh
+++ scpt/get_avatar_timeline.zsh
@@ -1,43 +1,44 @@
 
 host_meta=".well-known//webfinger?resource"
 
-touch $json_avatar
-rm $json_avatar
+if [ -f $json_avatar_timeline ];then
+	rm $json_avatar_timeline
+fi
 
 case $2 in
 	public)
-		turl=$protocol://$host/$api_url/timelines/public
+		turl="$protocol://$host/$api_url/timelines/public/?limit=40"
 	;;
 	*)
-		turl=$protocol://$host/$api_url/timelines/home
+		turl="$protocol://$host/$api_url/timelines/home/?limit=40"
 	;;
 esac
 
-tjson=`curl -sSL $turl -H "Authorization: Bearer $access_token"`
-tbody=`echo "$tjson"|jq -r '.[].account|{avatar,acct,url}'`
-tn=`echo "$tjson"| jq -r length`
+curl -sSL $turl -H "Authorization: Bearer $access_token" >! $json_timeline
+
+tjson=`cat $json_timeline`
+tbody=`cat $json_timeline|jq -r '.[].account|{avatar,acct,url}'`
+tn=`cat $json_timeline| jq -r length`
 if [ "$tn" != "0" ];then
 	tn=$((${tn} - 1))
 fi
-
 unset acct_tmp
 
 for ((i=0;i<=$tn;i++))
 do
-	acct=`echo $tjson|jq -r ".[$i].account|.acct"`
+	acct=`cat $json_timeline|jq -r ".[$i].account|.acct"`
 	if echo "$acct_tmp"|grep -v "$acct" > /dev/null 2>&1;then
-		avatar=`echo $tjson|jq -r ".[$i].account|.avatar"`
-		avatar_static=`echo $tjson|jq -r ".[$i].account|.avatar_static"`
-		url=`echo $tjson|jq -r ".[$i].account|.url"|cut -d / -f -3`
-		echo $url
+		avatar=`cat $json_timeline|jq -r ".[$i].account|.avatar"`
+		avatar_static=`cat $json_timeline|jq -r ".[$i].account|.avatar_static"`
+		url=`cat $json_timeline|jq -r ".[$i].account|.url"|cut -d / -f -3`
 		if [ "$url" != "null" ];then
 			output=`curl -sSL -H "Accept: application/json" "$url/${host_meta}=acct:${acct}"|jq -r '.links|.[]|select(.type == "application/atom+xml")|.href'`
 			img=`curl -sL $output | awk -vRS="</logo>" '/<logo>/{gsub(/.*<logo>|\n+/,"");print;exit}'`
 			if [ $? -eq 0 ] && [ -n "$img" ];then
 				if [ $i -eq 0 ] ;then
-					echo "[{\"url\":\"$avatar\",\"static\":\"$avatar_static\",\"img\":\"$img\"}" >> $json_avatar
+					echo "[{\"url\":\"$avatar\",\"static\":\"$avatar_static\",\"img\":\"$img\"}" >> $json_avatar_timeline
 				else
-					echo ",{\"url\":\"$avatar\",\"static\":\"$avatar_static\",\"img\":\"$img\"}" >> $json_avatar
+					echo ",{\"url\":\"$avatar\",\"static\":\"$avatar_static\",\"img\":\"$img\"}" >> $json_avatar_timeline
 				fi
 			fi
 		fi
@@ -45,6 +46,6 @@ do
 	fi
 done
 
-echo "]"  >> $json_avatar
+echo "]"  >> $json_avatar_timeline
 
-cat $json_avatar | jq .
+cat $json_avatar_timeline | jq .
