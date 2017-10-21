38b3905e65910e743d6d3629fe7a9753203b5f80
syui <syui@syui.cf>
2017-10-21 10:20:00 +0900

    fix

--- scpt/reply.zsh
+++ scpt/reply.zsh
@@ -3,16 +3,14 @@ url="$protocol://$host/$api_url/timelines/public/?limit=40"
 curl -sSL $url -H "Authorization: Bearer $access_token" >! $j/timelines_public.json
 api_option=statuses
 url=$protocol://$host/$api_url/$api_option
-
+reply_id="in_reply_to_id=`cat $json_timeline|jq -c  '.[]|{id,content}'|peco|cut -d '"' -f 4`"
 if [ "$api_option" = "statuses" ] && [ -n "$2" ];then
 	message="status=${@:2}"
 else
 	echo "send message :"
 	read message
 fi
-
 message="status=$message"
-reply_id="in_reply_to_id=`cat $json_timeline|jq -c  '.[]|{id,content}'|peco|cut -d '"' -f 4`"
 curl -F $message -F $reply_id -sS $url -H "Authorization: Bearer $access_token"
 . $s/timeline_latest.zsh
 exit
