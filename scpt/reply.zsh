062db0c46817fd96253bc5f2041af9d9275a5325
syui <syui@syui.cf>
2017-11-06 22:15:23 +0900

    fix reply

--- scpt/reply.zsh
+++ scpt/reply.zsh
@@ -13,18 +13,28 @@ api_option=statuses
 url=$protocol://$host/$api_url/$api_option
 reply_id=`cat $json_timeline|jq -c  '.[]|{id,content,url}'|peco|cut -d '"' -f 4`
 reply_user=`cat $json_timeline|jq -r -c  ".[]|select(.id == \"$reply_id\")|.account.acct"`
+reply_content=`cat $json_timeline|jq -r -c  ".[]|select(.id == \"$reply_id\")|.content"`
 echo $reply_id
 echo $reply_user
+echo $reply_content
 if [ "$api_option" = "statuses" ] && [ -n "$2" ];then
 	message="status=${@:2}"
 else
 	echo "send message : vim[Enter]"
 	read k
-	echo " @${reply_user}" >! $txt_message
+	echo "> ${reply_content}" >! $txt_message
+	echo " @${reply_user}" >> $txt_message
 	vim $txt_message
-	echo "upload[Enter]"
+	echo "upload[y]"
 	read k
-	message=`cat $txt_message`
+	case $k in
+		[yY])
+			message=`cat $txt_message`
+		;;
+		*)
+			exit
+		;;
+	esac
 fi
 message="status=$message"
 reply_id="in_reply_to_id=$reply_id"
--- scpt/reply_notify.zsh
+++ scpt/reply_notify.zsh
@@ -7,26 +7,40 @@ fi
 
 url="$url/notifications/?limit=30"
 curl -sSL $url -H "Authorization: Bearer $access_token"  | jq . >! $json_notification
-url="$protocol://$host/$api_url/timelines/public/?limit=40"
-curl -sSL $url -H "Authorization: Bearer $access_token" >! $json_timeline
+
+if [ `cat $json_notification| jq length` -eq 1 ];then
+	cat $json_notification| jq .
+fi
+cat $json_notification| jq '.[]|[.type,.account.url,.status.uri,.status.content]'
+echo enter
+read 
 api_option=statuses
 url=$protocol://$host/$api_url/$api_option
 reply_id=`cat $json_notification|jq -c  '.[].status|{id,content,url}'|peco|cut -d '"' -f 4`
-reply_user=`cat $json_notification|jq -r ".[]|select(.id == \"$reply_id\")"`
-reply_user=`echo $reply_user| jq -r .account.acct`
+reply_tmp=`cat $json_notification|jq -r ".[].status|select(.id == \"$reply_id\")"`
+reply_user=`echo $reply_tmp| jq -r .account.acct`
+reply_content=`echo $reply_tmp| jq -r .content`
 echo $reply_id
 echo $reply_user
-
+echo $reply_content
 if [ "$api_option" = "statuses" ] && [ -n "$2" ];then
 	message="status=${@:2}"
 else
 	echo "send message : vim[Enter]"
 	read k
-	echo " @${reply_user}" >! $txt_message
+	echo "> ${reply_content}" >! $txt_message
+	echo " @${reply_user}" >> $txt_message
 	vim $txt_message
-	echo "upload[Enter]"
+	echo "upload[y]"
 	read k
-	message=`cat $txt_message`
+	case $k in
+		[yY])
+			message=`cat $txt_message`
+		;;
+		*)
+			exit
+		;;
+	esac
 fi
 message="status=$message"
 reply_id="in_reply_to_id=$reply_id"
