2eb7793e843ee3457ec44262320e8f4a88d0ec90
syui <syui@users.noreply.github.com>
2017-11-15 12:20:30 +0900

    fix

--- scpt/status_backup.zsh
+++ scpt/status_backup.zsh
@@ -18,8 +18,8 @@ if [ -z "$2" ];then
 		fi
 	done >> ./index.html
 else
-	tmp_json=`curl -sL $2| jq .|sed '$d'`
-	echo $tmp_json >! ./index.html
+	curl -sL $2| jq . |sed '$d' >! ./index.back
+	cat ./index.back >! ./index.html
 	if [ $? -eq 1 ];then
 		echo error curl
 		exit
@@ -27,12 +27,11 @@ else
 	for ((i=0;i<=$n;i++))
 	do
 		id=`cat $json_account_status| jq ".[$i].id"`
-		nu=`echo $tmp_json| jq ".[]|select(.id == $id)"`
+		nu=`cat ./index.back| jq ".[]|select(.id == $id)"`
 		if [ -n "$nu" ];then
 			echo ,
 			cat $json_account_status| jq ".[$i]|{id,content}"
 		fi
-
 	done >> ./index.html
 	echo "]" >> ./index.html
 fi
