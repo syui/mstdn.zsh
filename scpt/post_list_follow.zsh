a5d40e88fb636d8048bdd58b26791e47c7bb0bb2
syui <syui@syui.cf>
2018-09-11 21:38:49 +0900

    fix list

--- scpt/post_list_follow.zsh
+++ scpt/post_list_follow.zsh
@@ -15,26 +15,29 @@ body=`curl -sS $url -H "Authorization: Bearer $access_token"`
 list_id=`echo $body|jq -r ".[]|.id,.title"|tr '\n' ','|sed -e 's/,$//g'`
 list_id=`echo $list_id| peco|cut -d , -f 1`
 
-api_option=accounts/$user_id/followers
+api_option=accounts/$user_id/following
 url=$protocol://$host/$api_url/$api_option
-if [ -f $json_follower ];then
-	url=$protocol://$host/$api_url/$api_option
-	tmp=`cat $json_follower | jq -r '.[].acct'`
+if [ -f $json_follow ];then
+	tmp=`cat $json_follow | jq -r '.[].acct'`
 	echo "$tmp"
-	echo "remove $json_follower"
+	echo "remove $json_follow"
 	echo "[y]"
 	read key
 	case $key in
 		[yY])
-			curl -sS $url -H "Authorization: Bearer $access_token" >! $json_follower
+			curl -sS $url -H "Authorization: Bearer $access_token" >! $json_follow
+			cat $json_follow
 		;;
 	esac
+else
+	curl -sS $url -H "Authorization: Bearer $access_token" >! $json_follow
+	cat $json_follow
 fi
 
-api_option=lists/$user_id/accounts
+api_option=lists/$list_id/accounts
 url=$protocol://$host/$api_url/$api_option
-tmp=`cat $json_follower | jq -r '.[].acct'`
-n=`cat $json_follower | jq length`
+tmp=`cat $json_follow | jq -r '.[].acct'`
+n=`cat $json_follow | jq length`
 n=$((${n} - 1))
 
 echo "
@@ -52,7 +55,8 @@ esac
 
 for ((i=0;i<=$n;i++))
 do
-	uri=`cat $json_follower|jq -r ".[$i].id"`
+	uri=`cat $json_follow|jq -r ".[$i].id"`
 	uri="account_ids=$uri"
-	curl -sS -F $uri $url -H "Authorization: Bearer $access_token"
+	echo curl -sS -F $uri $url -H "Authorization: Bearer $access_token"
+	#curl -sS -F $uri $url -H "Authorization: Bearer $access_token"
 done
